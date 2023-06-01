2d2cc925048391af6789b555e6a01f9f20515062
smilillo <sofiemilillo@gmail.com>
2023-06-01 15:50:52 -0400

    added to user model

--- models/User.js
+++ models/User.js
@@ -1,8 +1,20 @@
-const mongoose = require('mongoose');
+const { Schema, model } = require('mongoose')
 
-const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    email: { type: String, required: true },
-    thoughts: {},
-    friends: {}
-})
+const userSchema = new Schema({
+    username: { 
+        type: String, required: true 
+    },
+    email: { 
+        type: String, required: true 
+    },
+    thoughts: [{
+
+    }],
+    friends: [{
+
+    }]
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
