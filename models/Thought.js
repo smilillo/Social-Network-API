146c445664221c758bd6c5956424c3e2422e4157
smilillo <sofiemilillo@gmail.com>
2023-06-01 16:07:26 -0400

    updated index.js to include reaction

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,5 @@
-const { Schema, model } = require('mongoose')
-const reactionSchema = require('./Reaction')
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
 
 const thoughtSchema = new Schema({
     thoughtText: { 
