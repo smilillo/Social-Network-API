a0c92d685fff8ca1bd1c1c039d9440589601b650
smilillo <sofiemilillo@gmail.com>
2023-06-01 16:37:37 -0400

    added routes to userController

---
+++ controllers/userController.js
@@ -0,0 +1,56 @@
+const { ObjectId } = require('mongoose').Types;
+const { User, Thought } = require('../models');
+
+module.exports = {
+    // GET all users
+    getUsers(req, res) {
+        User.find()
+        .then((user) => res.json(user))
+        .catch((err) => res.status(500).json(err));
+    },
+    // GET single user
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+          .select('-__v')
+          .then((user) =>
+            !user
+              ? res.status(404).json({ message: 'No user with that ID' })
+              : res.json(user)
+          )
+          .catch((err) => res.status(500).json(err));
+      },
+    // POST new user
+    createUser(req, res) {
+        User.create(req.body)
+          .then((user) => res.json(user))
+          .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+          });
+      },
+    // PUT user by id
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+          { _id: req.params.userId },
+          { $set: req.body },
+          { runValidators: true, new: true }
+        )
+          .then((user) =>
+            !user
+              ? res.status(404).json({ message: 'No user with this id!' })
+              : res.json(user)
+          )
+          .catch((err) => res.status(500).json(err));
+      },
+    // DELETE user by id
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+          .then((user) =>
+            !user
+              ? res.status(404).json({ message: 'No user with this id' })
+              : Thought.deleteMany({ _id: { $in: user.thought } })
+          )
+          .then(() => res.json({ message: 'User and thoughts deleted!' }))
+          .catch((err) => res.status(500).json(err));
+      },
+}
